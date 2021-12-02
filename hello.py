00d6235b6a2b6f3dc6f08e67c2b6128097d5505b
Noah Gift <noah.gift@gmail.com>
2021-12-02 14:38:41 +0000

    adding initial project

---
+++ hello.py
@@ -0,0 +1,11 @@
+def capit(word):
+    """This capitalizes a word"""
+
+    print(f"You passed in this word: {word}")
+    work = word.capitalize()
+    print(f"This is the new version: {work}")
+    return work
+
+
+# var=1
+# var=var
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import capit
+
+
+def test_capit():
+    assert "Cherry" == capit("cherry")
